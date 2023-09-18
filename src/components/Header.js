cb921d6f68568341c00fbc9516b96b9e246f64a2
Ayush bhatt <124452247+devayush204@users.noreply.github.com>
2023-09-18 17:07:15 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+"use client"
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
@@ -10,8 +11,8 @@ export default function Header() {
   return (
     <header>
       <nav className="flex justify-between items-center h-12 px-4 shadow-md bg-gray-800 text-white">
-        <Link href="/" className="text-lg font-bold">
-          Amazon Shopping Cart
+        <Link href="/" className="text-lg font-extrabold">
+          Shopping Cart
         </Link>
         <div>
           <span className="cart-badge">
