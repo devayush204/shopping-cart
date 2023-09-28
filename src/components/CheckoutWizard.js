0827466105e881395a7d87daa0039b5bf16590f9
Ayush bhatt <124452247+devayush204@users.noreply.github.com>
2023-09-28 10:43:30 +0530

    Update CheckoutWizard.js

--- src/components/CheckoutWizard.js
+++ src/components/CheckoutWizard.js
@@ -1,6 +1,6 @@
 export default function CheckoutWizard({ activeStep = 0 }) {
   return (
-    <div className="mb-5 flex flex-wrap">
+    <div className="mb-5 flex ">
       {['User Login', 'Shipping Address', 'Payment Method', 'Place Order'].map(
         (step, index) => (
           <div
